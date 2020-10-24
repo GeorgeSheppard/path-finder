9aedd5fb58fb180527d3e8a1c3d2863a9817121d
George <georgesheppard98@gmail.com>
2020-10-24 15:53:31 +0100

    dynamically assigning a grid based on the size of the page

--- src/Canvas/canvas.jsx
+++ src/Canvas/canvas.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useEffect, useState } from "react";
 // import Square from "../Square/square";
 import Hexagon from "../Hexagon/hexagon"
 
@@ -9,11 +9,61 @@ import Hexagon from "../Hexagon/hexagon"
  * Information on hexagonal coordinates here
  * https://www.redblobgames.com/grids/hexagons/
  */
-
-// TODO: Probably best way to create the nice looking hexagon is with an SVG
-
 const Canvas = (props) => {
-  return <Hexagon />
+	const ref = useRef(null);
+	const width = 50;
+	const spacing = 5;
+	const [windowSize, setWindowSize] = useState({height: 0, width: 0})
+	
+	useEffect(() => {
+		console.log('width', ref.current.offsetWidth, ref.current.offsetHeight);
+		setWindowSize({
+			height: ref.current.offsetHeight,
+			width: ref.current.offsetWidth
+		})
+	}, []);
+
+	const grid = createGrid(windowSize, width, spacing);
+
+	const style = {
+		overflow: "hidden",
+		height: "100vh",
+		width: "100vw"
+	}
+
+	return (
+		<div ref={ref} style={style}>
+			{grid && grid.map(coord => {
+				const hexProps = {
+					width,
+					x: coord[0],
+					y: coord[1],
+					spacing
+				}
+		
+				return <Hexagon key={`${coord[0]}:${coord[1]}`} {...hexProps} />
+			})}
+		</div>
+	)
 };
+
+const createGrid = (windowSize, width, spacing) => {
+	console.log('windowSize', windowSize, 'width', width);
+
+	if (windowSize.height !== 0 && windowSize.width !== 0) {
+		const sizeX = Math.floor(windowSize.width/ (width + spacing));
+		const sizeY = Math.floor(windowSize.height/( 2 * Math.sqrt(3) / 3 * width));
+
+		const coords = [];
+
+		for (let i = 0; i < sizeX; i++) {
+			for (let j = 0; j < sizeY; j++) {
+				coords.push([i, j]);
+			}
+		}
+
+		return coords;
+	}
+}
 
 export default Canvas;
