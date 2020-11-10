549dc208b853e0054dea5d873f71ba6c5638daf6
George <georgesheppard98@gmail.com>
2020-11-10 21:17:14 +0000

    Added documentation to hexagonGrid and centred the grid

--- src/HexagonGrid/hexagonGrid.jsx
+++ src/HexagonGrid/hexagonGrid.jsx
@@ -1,62 +1,160 @@
-import React from "react";
+import React, { useState } from "react";
 import Hexagon, { hexagonStylingProps } from "../Hexagon/hexagon";
 
+// TODO: For a large number of hexagons the performance is poor, should add an useEffect
+// hook so that createGrid is only called when the hexagon props change
+// should make it so that pixel coordinates are calculated once and stored
+// effectively the only function that should be run on a new render provided the grid stays
+// the same is the hexagonStartingStates function
+/**
+ * Creates a grid of tesselating hexagons the size of the canvas object that
+ * contains it, can define the width of an individual hexagon, the border size,
+ * spacing between each hexagon
+ * @param {object} props
+ */
 const HexagonGrid = (props) => {
-    const { width = 50, borderWidth = 5, spacing = 5,
-		backgroundColor = "#64C7CC", ...other } = props
-		
-    const createGrid = (windowSize, width, spacing, borderWidth) => {
-        if (windowSize.height !== 0 && windowSize.width !== 0) {
-            const horizontalSpacing = width + spacing + 2 * borderWidth;
-            const sizeX = Math.floor((windowSize.width - width) / horizontalSpacing);
-            const spaceX = windowSize.width - (sizeX * horizontalSpacing + width);
-            
-            const verticalSpacing = Math.sqrt(3) / 2 * (spacing + width)
-            const sizeY = Math.floor((windowSize.height - Math.sqrt(3) / 3 * width) / verticalSpacing);
-            const spaceY = windowSize.height - (sizeY * verticalSpacing + Math.sqrt(3) / 3 * width)
-            
-            const coords = [];
-            
-            for (let i = 0; i < sizeX; i++) {
-                for (let j = 0; j < sizeY; j++) {
-                    coords.push([i, j]);
-                }
-            }
-            
-            return { coords, spaceX, spaceY }
+  const {
+    width = 50,
+    borderWidth = 5,
+    spacing = 5,
+    hexagonStates,
+    setHexagonStates,
+    selected,
+    siderWidth,
+  } = props;
+
+  // Width of the hexagon INCLUDES the borderwidth
+  const horizontalSpacing = width + spacing;
+  const verticalSpacing = (Math.sqrt(3) / 2) * width + spacing / 2;
+
+  // Sent to the hexagons to allow state change for drag click
+  const [mouseDown, setMouseDown] = useState(false);
+
+  const handleClick = (state) => {
+    return (event) => {
+      if (event.button === 0) {
+        setMouseDown(state);
+      }
+    };
+  };
+
+  /**
+   * Given the size of the window to display the grid, calculates the list of possible
+   * coordinates, the available border width for the grid, and the dimensions of the grid
+   * @param {int} windowSize
+   */
+  const createGrid = (windowSize) => {
+    if (windowSize.height !== 0 && windowSize.width !== 0) {
+      const sizeX = Math.floor(
+        (windowSize.width - siderWidth - horizontalSpacing / 2) /
+          horizontalSpacing
+      );
+
+      const sizeY = Math.floor(windowSize.height / verticalSpacing);
+
+      const offsetX =
+        (windowSize.width - siderWidth - (sizeX + 0.5) * horizontalSpacing) / 2;
+      const offsetY = (windowSize.height - (sizeY + 0.5) * verticalSpacing) / 2;
+
+      const coords = [];
+
+      for (let i = 0; i < sizeX; i++) {
+        for (let j = 0; j < sizeY; j++) {
+          coords.push([i, j]);
         }
+      }
+
+      return { coords, offsetX, offsetY, sizeX, sizeY };
+    }
+  };
+
+  /**
+   * Converts a coordinate into a pixel coordinate
+   * @param {int} x
+   * @param {int} y
+   */
+  const coordToPixels = (x, y) => {
+    let pixelsX = horizontalSpacing * x;
+    const pixelsY = verticalSpacing * y + (Math.sqrt(3) / 6) * width;
+    if (y % 2 === 1) {
+      pixelsX += horizontalSpacing / 2;
     }
 
-    // TODO: To centralise grid of hexagons properly need to make sure this returns exactly
-    // the right spacing, can then update other maths to account for it
-    const coordToPixels = (x, y, spacing, width) => {
-        let pixelsX = (width + spacing) * x;
-        let pixelsY = (width * Math.sqrt(3) / 2 + spacing * Math.sqrt(3) / 2) * y;
-        if (y % 2 === 1) {
-            pixelsX += width / 2 + spacing / 2
-        }
-    
-        return { pixelsX, pixelsY }
+    return { pixelsX, pixelsY };
+  };
+
+  /**
+   * Maps a grid specification of the form
+   * {
+   * 'wall': [[2, 0], [3, 5]],
+   * 'goal': [[1, 2]],
+   * 'start': [[3,  6]]
+   * }
+   * into an array that has the type of each individual hexagon on the grid
+   * @param {object} gridProps output of the createGrid function
+   * @param {*} hexagonStates grid specification
+   */
+  const hexagonStartingStates = (gridProps, hexagonStates) => {
+    if (gridProps) {
+      const hexagonStartingStates = new Array(gridProps.coords.length).fill(
+        "space"
+      );
+      const { sizeY } = gridProps;
+
+      Object.entries(hexagonStates).forEach((row) => {
+        const key = row[0];
+        row[1].forEach((coord) => {
+          hexagonStartingStates[coord[0] * sizeY + coord[1]] = key;
+        });
+      });
+
+      return hexagonStartingStates;
     }
+  };
 
-    const gridProps = createGrid(props.windowSize, width, spacing, borderWidth)
-    const hexagonProps = hexagonStylingProps({ width, borderWidth, backgroundColor });
+  const gridProps = createGrid(props.windowSize);
+  // Calculate the styling props once and then use it for all hexagons
+  const hexagonProps = hexagonStylingProps({
+    width,
+    borderWidth,
+  });
+  const parsedHexagonStates = hexagonStartingStates(gridProps, hexagonStates);
 
-    return (<div>
-        {gridProps && gridProps.coords.map(coord => {
-            const [x, y] = coord;
+  return (
+    <div onMouseDown={handleClick(true)} onMouseUp={handleClick(false)}>
+      {gridProps &&
+        gridProps.coords.map((coord, i) => {
+          const [x, y] = coord;
 
-            const transform = coordToPixels(x, y, spacing, width);
+          const transform = coordToPixels(x, y, spacing, width, borderWidth);
 
-            const { spaceX, spaceY } = gridProps;
-            
-            const style = {
-                transform: `translate(${transform.pixelsX}px, ${transform.pixelsY}px)`
-						}
-						
-            return <Hexagon key={`${x}:${y}`} style={style} {...{...hexagonProps, coord, ...other}} />
+          const { offsetX, offsetY } = gridProps;
+
+          // maps hexagons to correct coordinate and centre the entire grid
+          const style = {
+            transform: `translate(${transform.pixelsX + offsetX}px, ${
+              transform.pixelsY + offsetY
+            }px)`,
+          };
+
+          return (
+            <Hexagon
+              key={`${x}:${y}`}
+              style={style}
+              css={hexagonProps}
+              {...{
+                type: parsedHexagonStates[i],
+                coord,
+                selected,
+                mouseDown,
+                hexagonStates,
+                setHexagonStates,
+              }}
+            />
+          );
         })}
-    </div>)
-}
+    </div>
+  );
+};
 
 export default HexagonGrid;
--- src/Toolbar/toolbar.jsx
+++ src/Toolbar/toolbar.jsx
@@ -14,33 +14,73 @@ const { SubMenu } = Menu;
 const { Sider } = Layout;
 
 const Toolbar = () => {
-	const [hexagonStates, setHexagonStates] = useState({
-		start: [],
-		goal: [],
-		wall: [],
-	})
-	const [selected, setSelected] = useState('wall')
+  const [hexagonStates, setHexagonStates] = useState({
+    goal: [[7, 3]],
+    start: [[4, 5]],
+    wall: [
+      [8, 2],
+      [5, 4],
+      [6, 7],
+      [5, 5],
+      [6, 4],
+      [7, 4],
+      [6, 5],
+      [7, 6],
+      [4, 3],
+      [4, 2],
+      [3, 2],
+      [7, 7],
+      [8, 6],
+      [8, 5],
+    ],
+  });
+
+  const [selected, setSelected] = useState("wall");
+
+  const siderWidth = 200;
 
   return (
     <Layout>
       <Layout>
-        <Sider width={200} className="site-layout-background">
-          <Menu
-						mode="inline"
-            style={{ height: "100vh" }}
-          >
-            <SubMenu key="sub1" icon={<UserOutlined />} title="Required" style={{
-							color: (hexagonStates.start.length === 0 || hexagonStates.goal.length === 0) && "red"
-						}}>
-              <Menu.Item key="1" onClick={() => setSelected('start')} style={{
-								color: hexagonStates.start.length === 0 && "red"
-							}}>Start</Menu.Item>
-              <Menu.Item key="2" onClick={() => setSelected('goal')} style={{
-								color: hexagonStates.goal.length === 0 && "red"
-							}}>Goal</Menu.Item>
+        <Sider width={siderWidth} className="site-layout-background">
+          <Menu mode="inline" style={{ height: "100vh" }}>
+            <SubMenu
+              key="sub1"
+              icon={<UserOutlined />}
+              title="Required"
+              style={{
+                color:
+                  (hexagonStates.start.length === 0 ||
+                    hexagonStates.goal.length === 0) &&
+                  "red",
+              }}
+            >
+              <Menu.Item
+                key="1"
+                onClick={() => setSelected("start")}
+                style={{
+                  color: hexagonStates.start.length === 0 && "red",
+                }}
+              >
+                Start
+              </Menu.Item>
+              <Menu.Item
+                key="2"
+                onClick={() => setSelected("goal")}
+                style={{
+                  color: hexagonStates.goal.length === 0 && "red",
+                }}
+              >
+                Goal
+              </Menu.Item>
             </SubMenu>
-            <SubMenu key="sub2" icon={<LaptopOutlined />} title="Optional" >
-              <Menu.Item key="3" onClick={() => setSelected('wall')}>Wall</Menu.Item>
+            <SubMenu key="sub2" icon={<LaptopOutlined />} title="Optional">
+              <Menu.Item key="3" onClick={() => setSelected("wall")}>
+                Wall
+              </Menu.Item>
+              <Menu.Item key="4" onClick={() => setSelected("space")}>
+                Space
+              </Menu.Item>
             </SubMenu>
             <SubMenu
               key="sub3"
@@ -54,10 +94,18 @@ const Toolbar = () => {
             </SubMenu>
           </Menu>
         </Sider>
-            <Canvas Component={HexagonGrid} {...{selected, hexagonStates, setHexagonStates}} />
+        <Canvas
+          Component={HexagonGrid}
+          {...{
+            selected,
+            hexagonStates,
+            setHexagonStates,
+            siderWidth,
+          }}
+        />
       </Layout>
     </Layout>
   );
-}
+};
 
 export default Toolbar;
